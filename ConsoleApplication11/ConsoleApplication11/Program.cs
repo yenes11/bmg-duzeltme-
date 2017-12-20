42dc9e81e27f00d2ca3468a58a5b898a6466fb17
yenes11 <34625410+yenes11@users.noreply.github.com>
2017-12-20 10:57:08 +0300

    Add files via upload

---
+++ ConsoleApplication11/ConsoleApplication11/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a;
+            for (; ; )
+            {
+                Console.WriteLine("Adınızı Giriniz:");
+                a = Console.ReadLine();
+                foreach (char b in a)
+                    Console.Write((char)(b + 10));
+                Console.WriteLine();
+                Console.WriteLine("Sıfırlamak için herhangi bir tuşa basın.");
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+    }
+}
